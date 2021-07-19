cf0f0ca2c24af41ea5b2fc3c5424b44952958deb
lihong <963719795@qq.com>
2021-07-19 14:01:47 +0800

    fix: 修复element-ui引入

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './plugins/element.js'
+import './plugins/element-ui.js'
 
 Vue.config.productionTip = false
 
