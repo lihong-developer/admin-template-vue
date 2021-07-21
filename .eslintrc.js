663dca08d346eb46f9641534bab8129b90b75942
lihong <963719795@qq.com>
2021-07-21 13:54:34 +0800

    feat:添加格式化代码prettier配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ["warn", 4],
+    'vue/html-closing-bracket-newline': 'off',
+    "vue/html-indent": ["warn", 4],
+    "vue/html-self-closing": 'off',
+    "vue/max-attributes-per-line": 'off'
   }
 }
